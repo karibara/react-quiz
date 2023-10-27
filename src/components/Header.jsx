ca51dd98d1a8b5dc8a99e555f42e5da69b62553b
malgorzata bozykowska <malgorzata.bozykowska@gmail.com>
2023-10-27 09:02:38 +0200

    Add Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
-export default function Header () {
-    return <header>
-        <img />
+import logoImg from '../assets/quiz-logo.png';
+
+export default function Header() {
+    return (
+        <header>
+        <img scr={logoImg} alt="Agatha Christie Quiz logo"/>
         <h1>'Queen of Crime' QUIZ</h1>
     </header>
+        ) 
 }
