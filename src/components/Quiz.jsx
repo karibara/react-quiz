e5894a596cdd5d06e561ce8843bceb604358bea0
malgorzata bozykowska <malgorzata.bozykowska@gmail.com>
2023-10-30 10:19:34 +0100

    Add styling

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,13 +1,31 @@
 import { useState } from "react"
+import QUESTIONS from "../questions"
 
 export default function Quiz() {
-    // currently active question is managed throw the index. In array of questions we manage the index of the currently displayed question, and change the index whenever the user answered a question.
-    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0)
-
-    // Register answers in an array
+    // Register answers in an array. The number of stored answers in this array is currently question index (because index starts at 0)
     const [userAnswers, setUserAnswers] = useState([])
 
+    const activeQuestionIndex = userAnswers.length
+
+    function handleSelectAnswer(selectedAnswer) {
+        setUserAnswers((prevUserAnswers) => {
+            return [...prevUserAnswers, selectedAnswer];
+        });
+    }
+
     return (
-        <p>Currently active question</p>
+        <div id="quiz">
+
+        <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+                <button onClick={() => {handleSelectAnswer()}}>{answer}</button>
+            </li>
+        ))}
+        </ul>
+        </div>
+        </div>
     )
 }
